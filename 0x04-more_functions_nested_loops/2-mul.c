ec2bf5c20501994acc52f2e62176a76feab6bdf8
Zaiyyo <azaynab.2019@gmail.com>
2023-08-18 19:24:14 -0700

    tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*mul - function that multiplies two numbers
+*@a: first number
+*@b: second number
+*Return: return result
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
