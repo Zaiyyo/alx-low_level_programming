157a068e35e002771e60ba4561b71d73b812c171
Zaiyyo <azaynab.2019@gmail.com>
2023-08-21 16:50:15 -0700

    task no. 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Write a function that takes a pointer as parameter and updates the value 
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n);
+{
+	*n = 98
+}
