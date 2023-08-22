cca305f35304dfbd4066359d6e6eb3f6d4e58bf5
Zaiyyo <azaynab.2019@gmail.com>
2023-08-21 18:11:46 -0700

    task no. 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to integer
+ * @s: string to convert
+ *
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, di;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	di = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			di = s[i] - '0';
+			if (d % 2)
+				di = -di;
+			n = n * 10 + di;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
