c73ef97be8448a7c340a1fcf362e8e87886c81bc
Zaiyyo <azaynab.2019@gmail.com>
2023-08-31 18:46:48 -0700

    task no. 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,8 +3,8 @@
 
 /**
  * is_palindrome - checks if a string is a palindrome
- * @s: string to reverse
- * Return: 1 if it is, 0 it's not
+ * @s: string
+ * Return: 0
  */
 int is_palindrome(char *s)
 {
