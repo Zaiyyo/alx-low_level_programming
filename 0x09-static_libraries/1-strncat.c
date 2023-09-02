6735cee5f646c56ba1fc4620020ac131f9b1322c
Zaiyyo <azaynab.2019@gmail.com>
2023-09-01 19:32:15 -0700

    task no. 0 agaain

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,15 +1,31 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * _strncat - a function
- * @dest: something
- * @src: something
- * @n: something
+ * _strncat - concatenates two strings
+ * @dest: input parameter string
+ * @src: input parameter string
+ * @n: times to iterations
  *
- * Return: 0
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int a;
+	int b;
 
+	a = 0;
+
+	while (dest[a] != 0)
+	{
+		a++;
+	}
+
+	b = 0;
+
+	while (src[b] != 0 && b < n)
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	return (dest);
 }
