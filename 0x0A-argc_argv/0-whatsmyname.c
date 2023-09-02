3cae459427be7179eca16e33b5c5060ca85678ad
Zaiyyo <azaynab.2019@gmail.com>
2023-09-01 18:32:40 -0700

    task no. 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,26 +1,13 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - a function that prints the number of arguments passed into main
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0
+ * main - a function that prints the name of the file
+ * @argc: is an argument counter for char
+ * @argv: is an argument value for char
+ * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-	int i;
-
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			;
-
-		printf("%d\n", i - 1);
-	}
-
+	printf("%s\n", argv[argc - argc]);
 	return (0);
 }
