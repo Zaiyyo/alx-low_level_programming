bd5fc88d6f79467385723eae9317bc22ab37a6cc
Zainab <azaynab.2019@gmail.com>
2023-12-22 18:21:52 -0800

    tasks..

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,31 +1,40 @@
 #include "hash_tables.h"
+#include <stdio.h>
+#include <string.h>
+
+int not_last_node(const hash_table_t *ht, hash_node_t *node);
 
 /**
- * hash_table_print - prints the keys and values of the hash table
- *
- * @ht: pointer to the hash table
- * Return: no return
+ * hash_table_print - prints the contents of a hash table
+ * @ht: hash table to print
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i;
-	hash_node_t *tmp;
-	char *sep;
+	unsigned long int i = 0, last_pos = 0;
+	hash_node_t *node = NULL;
 
 	if (ht == NULL)
 		return;
-
-	printf("{");
-	sep = "";
-
-	for (i = 0; i < ht->size; i++)
+	putchar('{');
+	if (ht)
 	{
-		tmp = ht->array[i];
-		while (tmp != NULL)
+		for (; i < ht->size - 1; i++)
 		{
-			printf("%s'%s': '%s'", sep, tmp->key, tmp->value);
-			sep = ", ";
-			tmp = tmp->next;
+			if (ht->array[i] != NULL)
+				last_pos = i;
+		}
+
+		for (i = 0; i <= last_pos; i++)
+		{
+			node = ht->array[i];
+			while (node)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (i < last_pos - 1)
+					printf(", ");
+			}
+
 		}
 	}
 	printf("}\n");
